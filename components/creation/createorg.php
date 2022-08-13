b8d28e07ab147aa5ad201e6ea51205bf7b71bfc3
camsdono <68128526+camsdono@users.noreply.github.com>
2022-08-13 19:10:23 +0100

    V0.01 - Favicon added
    Added a favicon temporary which will  be there for a bit.  Also just made one change to the UI

--- components/creation/createorg.php
+++ components/creation/createorg.php
@@ -22,6 +22,11 @@ if(!$_SESSION['username'] == null) {
         <meta name="author" content="Camsdono Studios">
         <meta name="description" content="A better place to keep track of your bugs and manage teams">
 
+        <link rel="apple-touch-icon" sizes="180x180" href="../../images/favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="../../images/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="../../images/favicon/favicon-16x16.png">
+        <link rel="manifest" href="../../images/favicon/site.webmanifest">
+
         <link rel="stylesheet" href="../../styles/styles.css" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
