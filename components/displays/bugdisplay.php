4a62b4a12315319d07c612b5783284753b283248
camsdono <68128526+camsdono@users.noreply.github.com>
2022-08-24 21:40:53 +0100

    V0.01 - Bug Display page created
    Need to add functionality to actually display bug data.

---
+++ components/displays/bugdisplay.php
@@ -0,0 +1,57 @@
+<?php
+
+require('../../backend/config.php');
+
+session_start();
+
+if(!$_SESSION['username'] == null) {
+    $username = $_SESSION['username'];
+} else {
+    header("Location: ../auth/login.php");
+}
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title id="title"></title>
+
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width; initial-scale=1; maximum-scale=1; user-scalable=0">
+        <meta name="author" content="Camsdono Studios">
+        <meta name="description" content="A better place to keep track of your bugs and manage teams">
+
+        <link rel="apple-touch-icon" sizes="180x180" href="../../images/favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="../../images/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="../../images/favicon/favicon-16x16.png">
+        <link rel="manifest" href="../../images/favicon/site.webmanifest">
+
+        <link rel="stylesheet" href="../../styles/styles.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <body>
+        <section class="blue">
+            <div class="curve"></div>
+            <div class="topnav" id="myTopnav">
+                <a href="../root/home.php">Home</a>
+                <a href="../root/organization.php">Organizations</a>
+                <a href="#">Tickets</a>
+
+                <a href="javascript:void(0);" class="icon" onclick="OpenCloseNav()">
+                    <i class="fa fa-bars"></i>
+                </a>
+            </div>
+
+            <ul class="breadcrumbs">
+                <li class="breadcrumbs-item">
+                    <a href="../root/organization.php?id=<?=$row['orgID']?>" class="breadcrumbs-link">Your Organizations</a>
+                </li>
+                <li class="breadcrumbs-item">
+                    <a href="./orgdisplay.php?id=<?=$row['orgID'] ?>" class="breadcrumbs-link"><?=$row['orgName']?></a>
+                </li>
+            </ul>
+        </section>
+    </body>
+</html>
+
