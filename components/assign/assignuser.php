ce1089ae6e744095641df40fc7f7add4213e25a6
camsdono <68128526+camsdono@users.noreply.github.com>
2022-08-26 22:13:54 +0100

    V0.01 - Assign Users WIP
    Started work on the assign users page 
    /* <select name="species[]" multiple > */
    
    /* foreach ($species as $s) {
        $query_rsCatch = "INSERT INTO SpeciesHunt 
            (DateCaught, CatchYear, BoatName, BoatMake, Species, Angler, Skipper, Notes, PhotoName, Photo) 
            VALUES('$datecaught','$year','$boatname','$boatmake','$s','$angler','$skipper','$notes','$imagename','$imagetmp')";
        $rsCatch = mysql_query($query_rsCatch, $webdb) or die(mysql_error());
    } /* - code online for what i want to do.

---
+++ components/assign/assignuser.php
@@ -0,0 +1,62 @@
+<?php
+
+require('../../backend/config.php');
+
+session_start();
+
+if(!$_SESSION['username'] == null) {
+    $username = $_SESSION['username'];
+} else {
+    header("Location: ../auth/login.php");
+}
+
+$bugid = $_GET['id'];
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+        <title id="title">Assign User</title>
+
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width; initial-scale=1; maximum-scale=1; user-scalable=0">
+        <meta name="author" content="Camsdono Studios">
+        <meta name="description" content="A better place to keep track of your bugs and manage teams">
+
+        <link rel="apple-touch-icon" sizes="180x180" href="../../images/favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="../../images/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="../../images/favicon/favicon-16x16.png">
+        <link rel="manifest" href="../../images/favicon/site.webmanifest">
+
+        <link rel="stylesheet" href="../../styles/styles.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <body>
+        <section class="blue">
+            <div class="curve"></div>
+            <div class="topnav" id="myTopnav">
+                <a href="../root/home.php">Home</a>
+                <a href="../root/organization.php">Organizations</a>
+                <a href="#">Tickets</a>
+
+                <a href="javascript:void(0);" class="icon" onclick="OpenCloseNav()">
+                    <i class="fa fa-bars"></i>
+                </a>
+            </div>
+
+            <h2>Assign User To Bug</h2>
+
+            <div class="back-button" onclick="window.location = '../displays/projectdisplay.php?id=<?=$projectid?>&page=1'">
+                <h4 class="back-button-item">Back</h4>
+            </div>
+
+            <form method="POST" action="../../backend/createprocesses/createbugprocess.php">
+                
+            </form>
+        </section>
+        <footer>
+            <p class="footer-txt">@Camsdono Studios</p>
+        </footer>
+    </body>
+</html>
