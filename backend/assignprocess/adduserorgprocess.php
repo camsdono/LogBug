d87155bc65438f83c6a936c8cefd95ca681ae246
camsdono <cammie2006@outlook.com>
2023-03-01 13:13:11 +0000

    Patch 2 Updated Org process

--- backend/assignprocess/adduserorgprocess.php
+++ backend/assignprocess/adduserorgprocess.php
@@ -1,46 +1,39 @@
 <?php
-
 require('../config.php');
 
-
 if (isset($_POST["org-assign-user-btn"])) {
-    session_start();
-
     $username = $_POST['username'];
     $userRole = $_POST['userRole'];
     $orgid = $_POST['orgid'];
     $orgname = $_POST['orgname'];
-    $assignCode = substr(md5(uniqid(mt_rand(), true)) , 0, 8);
+    $assignCode = substr(md5(uniqid(mt_rand(), true)), 0, 8);
 
     $getUser = "SELECT * FROM users WHERE username = '$username'";
     $getUserResult = mysqli_query($conn, $getUser);
 
-    if(mysqli_num_rows($getUserResult) > 0) {
+    if (mysqli_num_rows($getUserResult) > 0) {
         $getUserRow = mysqli_fetch_array($getUserResult);
-
         $getUserID = $getUserRow['id'];
 
         $getUserData = "SELECT * FROM org_members WHERE memberID=$getUserID AND orgID=$orgid";
         $getUserDataResult = mysqli_query($conn, $getUserData);
 
-        if(mysqli_num_rows($getUserDataResult) < 1) {
+        if (mysqli_num_rows($getUserDataResult) < 1) {
             $stmt = $conn->prepare("INSERT INTO org_members (orgName, orgID, orgMember, memberID, orgRole, assignCode) VALUES (?, ?, ?, ?, ?, ?)");
             $stmt->bind_param("sisiss", $orgname, $orgid, $username, $getUserID, $userRole, $assignCode);
-    
-            $stmt->execute();
-            $res = mysqli_stmt_get_result($stmt);
 
-            if(!$res) {
+            if ($stmt->execute()) {
                 header("Location: ../../components/assign/adduserorg.php?id=$orgid");
             } else {
-                echo "An error has occured adding user to org try again later.";
+                echo "An error has occurred adding user to org: " . mysqli_error($conn);
             }
-        
+
             $stmt->close();
         } else {
             echo "User already in org!";
         }
+    } else {
+        echo "User does not exist!";
     }
 }
-
 ?>
