a576ac511ef1bb0c0f4111ee473398f911a38c42
camsdono <cammie2006@outlook.com>
2023-03-02 20:24:24 +0000

    Navbar WIP

--- components/root/home.php
+++ components/root/home.php
@@ -33,33 +33,124 @@ $getBugsRes = $conn->query($getBugs);
         <link rel="icon" type="image/png" sizes="16x16" href="../../images/favicon/favicon-16x16.png">
         <link rel="manifest" href="../../images/favicon/site.webmanifest">
 
-        <link rel="stylesheet" href="../../styles/styles.css" />
+        <!---<link rel="stylesheet" href="../../styles/styles.css" /> -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
-    <body  class="blue">
+    <style>
+        body {
+            margin: 0;
+            font-family: 'Prompt', sans-serif;
+            color: white;
+            /*background: #202731; */
+            overflow-x: hidden;
+        }
+
+        .profile-nav {
+            display: flex;
+            justify-content: flex-end;
+            align-items: center;
+            height: 60px;
+            padding: 0 20px;
+            background-color: #f5f5f5;
+        }
+
+        .dropdown-toggle {
+            display: flex;
+            align-items: center;
+            background-color: transparent;
+            border: none;
+            outline: none;
+            cursor: pointer;
+        }
+
+        .profile-image {
+            width: 35px;
+            height: 35px;
+            border-radius: 50%;
+            margin-right: 10px;
+        }
+
+        .profile-name {
+            font-size: 16px;
+            font-weight: bold;
+        }
+
+        .dropdown-menu {
+            width: 150px;
+            background-color: #f5f5f5;
+            display: none;
+            flex-direction: column;
+            position: absolute;
+            right: 0;
+            text-align: center;
+            z-index: 1;
+        }
+
+        .dropdown-menu .dropdown-item:first-child {
+            border-top-left-radius: 5px;
+            border-top-right-radius: 5px;
+        }
+        .dropdown-menu .dropdown-item:last-child {
+            border-bottom-left-radius: 5px;
+            border-bottom-right-radius: 5px;
+        }
+
+        .dropdown-item {
+            font-size: 16px;
+            padding: 12px 30px;
+            color: #000;
+            text-decoration: none;
+        }
+
+        .dropdown-item:hover {
+            background-color: #ddd;
+        }
+
+        .icon {
+            display: none;
+        }
+
+        /* Mobile */
+        @media screen and (max-width: 600px) {
+            .icon {
+                display: block;
+                position: absolute;
+                top: 0;
+                left: 0;
+                transform: translate(25%, 50%);
+                font-size: 1.8rem;
+                cursor: pointer;
+            }
+
+            .icon {
+                color: black;
+            }
+
+            .profile-nav {
+                display: none;
+            }
+        }
+    </style>
+
+    <body>
     <nav class="profile-nav">
         <div class="dropdown">
             <button class="dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <img class="profile-image" src="https://via.placeholder.com/50x50" alt="Profile Image">
-            <span class="profile-name">John Doe</span>
+                <img class="profile-image" src="https://via.placeholder.com/50x50" alt="Profile Image">
+                <span class="profile-name"><?=htmlspecialchars($username)?></span>
             </button>
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <a class="dropdown-item" href="#">Profile</a>
-            <a class="dropdown-item" href="#">Settings</a>
-            <a class="dropdown-item" href="#">Logout</a>
+            <div class="dropdown-menu" id="menu" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item" href="#">Profile</a>
+                <a class="dropdown-item" href="#">Settings</a>
+                <a class="dropdown-item" href="../../backend/auth/logout.php">Logout</a>
             </div>
         </div>
     </nav>
-           
-            <a href="../displays/notificationdisplay.php"><i alt="notification-ico" class="fa fa-solid fa-bell"></i></a>
-
             <a href="javascript:void(0);" class="icon" onclick="OpenCloseNav()">
                 <i class="fa fa-bars"></i>
             </a>
         </div>
-        <div class="welcome-message">
-            <h2 style="text-align: center;">Welcome <?=htmlspecialchars($username)?></h2>
-        </div>
+
         <div class="info-row">
             <h4 style="margin-left: 5px;">Stats:</h4>
             <div class="info">
@@ -78,3 +169,20 @@ $getBugsRes = $conn->query($getBugs);
     </body>
 </html>
 <script src="../../js/openCloseNavBar.js"></script>
+<script>
+    document.querySelector('.dropdown-toggle').addEventListener('mouseover', function() {
+        document.querySelector('.dropdown-menu').style.display = 'flex';
+    });
+    document.querySelector('.dropdown-menu').addEventListener('mouseover', function() {
+        document.querySelector('.dropdown-menu').style.display = 'flex';
+    });
+
+    document.querySelector('.dropdown-toggle').addEventListener('mouseleave', function() {
+        document.querySelector('.dropdown-menu').style.display = 'none';
+    });
+
+    document.querySelector('.dropdown-menu').addEventListener('mouseleave', function() {
+        document.querySelector('.dropdown-menu').style.display = 'none';
+    });
+
+</script>
