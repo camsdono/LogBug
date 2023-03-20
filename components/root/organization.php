d41de9e34719cf9dc381bdc1b329724af25286b0
camsdono <cammie2006@outlook.com>
2023-03-20 22:03:27 +0000

    new ui login page

--- components/root/organization.php
+++ components/root/organization.php
@@ -149,52 +149,7 @@ $pendingOrgs = $conn->query($getPendingOrgs);
         </pop-up>
     </body>
 </html>
+
+<script src="../../js/displays/createorgDisplay.js"></script>
 <script src="../../js/openCloseNavBar.js"></script>
 <script src="../../js/changeTheme.js"></script>
-<script>
-    var popup = document.getElementById("pop-up");
-    var main = document.getElementById("main");
-    var orgbutton = document.getElementById("org-create");
-    var closebutton = document.getElementsByClassName("close-button")[0];
-    var nav = document.getElementById("nav");
-    var orgName = document.getElementsByName("orgName")[0];
-    var orgDesc = document.getElementsByName("orgDesc")[0];
-
-    orgbutton.addEventListener("click", function() {
-        popup.style.display = "block";
-        main.style.filter = "blur(5px)";
-        nav.style.filter = "blur(5px)";
-    });
-
-    closebutton.addEventListener("click", function() {
-        popup.style.display = "none";
-        main.style.filter = "blur(0px)";
-        nav.style.filter = "blur(0px)";
-        ClearInputs();
-    });
-
-    // When the user clicks anywhere outside of the modal, close it if it is open
-    window.onclick = function(event) {
-        if (event.target == popup) {
-            popup.style.display = "none";
-            main.style.filter = "blur(0px)";
-            nav.style.filter = "blur(0px)";
-            ClearInputs();
-        }
-    }
-
-    // if the user presses escape close the modal
-    document.addEventListener('keydown', function(event) {
-        if (event.key === "Escape") {
-            popup.style.display = "none";
-            main.style.filter = "blur(0px)";
-            nav.style.filter = "blur(0px)";
-            ClearInputs();
-        }
-    });
-
-    function ClearInputs() {
-        orgName.value = "";
-        orgDesc.value = "";
-    }
-</script>
